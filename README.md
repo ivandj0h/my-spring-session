c2a92d3378f2108bcd015dd3ae617b0cc2966e3d
ivandi1980 <ivandi.djoh@gmail.com>
2023-05-04 21:31:01 +0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Spring Session
+
+## Description
+`My Spring Session` is a simple Spring Boot application that demonstrates how to use Spring Session to manage user sessions.
+
+`This Project is under development. kindly check back later.`
---
+++ src/main/java/com/ivandjoh/springsessiom/SpringSessiomApplication.java
@@ -0,0 +1,13 @@
+package com.ivandjoh.springsessiom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessiomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSessiomApplication.class, args);
+	}
+
+}
